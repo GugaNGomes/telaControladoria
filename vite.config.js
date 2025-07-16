d1b3716d7527d3043f2e9cda4269b83830cbf62b
Gustavo Gomes do Nascimento <gustavo.nascimento@postall-log.com.br>
2025-07-16 14:44:28 -0300

    erro de autoriação

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,14 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      '/api/controladoria': {
+        target: 'https://www.postallweb.com.br/homolog',
+        changeOrigin: true,
+        secure: false,
+        rewrite: (path) => path.replace(/^\/api\/controladoria/, '/api/controladoria'),
+      },
+    },
+  },
 })
